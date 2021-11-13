492b04ac896d0caf5c3c165b0276a5276fbccb75
Rizqi Cahya Angelita <90441810+rizqicahyaa@users.noreply.github.com>
2021-11-13 10:49:18 +0700

    Add files via upload

---
+++ Rizqi Cahya Angelita_047_permasalahan 23.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+
+void garis();
+int main(){
+	garis();
+	cout<<"\n NO	NIM	NAMA	NILAI \n";
+	garis();
+	getch();
+}
+void garis()
+{
+	cout<<"==============================";
+}
